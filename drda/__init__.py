083e4be8c040794226960c0abec63b3aaaf0a0ca
Hajime Nakagami <nakagami@gmail.com>
2016-08-19 13:45:36 +0900

    default user (schema) APP

--- drda/__init__.py
+++ drda/__init__.py
@@ -122,6 +122,6 @@ class NotSupportedError(DatabaseError):
         DatabaseError.__init__(self, 'NotSupportedError')
 
 
-def connect(host, database, port, user='anonymous', password=None):
+def connect(host, database, port, user='APP', password=None):
     return Connection(host, database, port, user, password)
 
