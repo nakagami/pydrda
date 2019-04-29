57b839aa7be59b9ad96ccb155dfc80c2dffa914c
Hajime Nakagami <nakagami@gmail.com>
2019-04-29 11:12:30 +0900

    update copyright year

--- drda/connection.py
+++ drda/connection.py
@@ -1,7 +1,7 @@
 ##############################################################################
 # The MIT License (MIT)
 #
-# Copyright (c) 2016-2017 Hajime Nakagami
+# Copyright (c) 2016-2019 Hajime Nakagami<nakagami@gmail.com>
 #
 # Permission is hereby granted, free of charge, to any person obtaining a copy
 # of this software and associated documentation files (the "Software"), to deal
