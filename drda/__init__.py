c99c9d8f4116939010ea425ba202ca54984ebea4
Hajime Nakagami <nakagami@gmail.com>
2024-02-06 17:52:10 +0900

    bump up version

--- drda/__init__.py
+++ drda/__init__.py
@@ -26,7 +26,7 @@ import datetime
 import decimal
 from .connection import Connection
 
-VERSION = (0, 4, 8)
+VERSION = (0, 5, 0)
 __version__ = '%s.%s.%s' % VERSION
 apilevel = '2.0'
 threadsafety = 1
